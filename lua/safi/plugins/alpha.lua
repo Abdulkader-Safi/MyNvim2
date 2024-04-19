eb373ed212bde9102be5581e31faba3410999be6
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-04-19 20:39:01 +0300

    update alpha dashboard and setup lsp mason with lsp config

--- lua/safi/plugins/alpha.lua
+++ lua/safi/plugins/alpha.lua
@@ -7,28 +7,29 @@ return {
 
     -- Set header
     dashboard.section.header.val = {
-"   SSSSSSSSSSSSSSS                      ffffffffffffffff    iiii  ",
-" SS:::::::::::::::S                    f::::::::::::::::f  i::::i ",
-"S:::::SSSSSS::::::S                   f::::::::::::::::::f  iiii  ",
-"S:::::S     SSSSSSS                   f::::::fffffff:::::f        ",
-"S:::::S              aaaaaaaaaaaaa    f:::::f       ffffffiiiiiii ",
-"S:::::S              a::::::::::::a   f:::::f             i:::::i ",
-" S::::SSSS           aaaaaaaaa:::::a f:::::::ffffff        i::::i ",
-"  SS::::::SSSSS               a::::a f::::::::::::f        i::::i ",
-"    SSS::::::::SS      aaaaaaa:::::a f::::::::::::f        i::::i ",
-"       SSSSSS::::S   aa::::::::::::a f:::::::ffffff        i::::i ",
-"            S:::::S a::::aaaa::::::a  f:::::f              i::::i ",
-"            S:::::Sa::::a    a:::::a  f:::::f              i::::i ",
-"SSSSSSS     S:::::Sa::::a    a:::::a f:::::::f            i::::::i",
-"S::::::SSSSSS:::::Sa:::::aaaa::::::a f:::::::f            i::::::i",
-"S:::::::::::::::SS  a::::::::::aa:::af:::::::f            i::::::i",
-" SSSSSSSSSSSSSSS     aaaaaaaaaa  aaaafffffffff            iiiiiiii",
- }
+      "   SSSSSSSSSSSSSSS                      ffffffffffffffff    iiii  ",
+      " SS:::::::::::::::S                    f::::::::::::::::f  i::::i ",
+      "S:::::SSSSSS::::::S                   f::::::::::::::::::f  iiii  ",
+      "S:::::S     SSSSSSS                   f::::::fffffff:::::f        ",
+      "S:::::S              aaaaaaaaaaaaa    f:::::f       ffffffiiiiiii ",
+      "S:::::S              a::::::::::::a   f:::::f             i:::::i ",
+      " S::::SSSS           aaaaaaaaa:::::a f:::::::ffffff        i::::i ",
+      "  SS::::::SSSSS               a::::a f::::::::::::f        i::::i ",
+      "    SSS::::::::SS      aaaaaaa:::::a f::::::::::::f        i::::i ",
+      "       SSSSSS::::S   aa::::::::::::a f:::::::ffffff        i::::i ",
+      "            S:::::S a::::aaaa::::::a  f:::::f              i::::i ",
+      "            S:::::Sa::::a    a:::::a  f:::::f              i::::i ",
+      "SSSSSSS     S:::::Sa::::a    a:::::a f:::::::f            i::::::i",
+      "S::::::SSSSSS:::::Sa:::::aaaa::::::a f:::::::f            i::::::i",
+      "S:::::::::::::::SS  a::::::::::aa:::af:::::::f            i::::::i",
+      " SSSSSSSSSSSSSSS     aaaaaaaaaa  aaaafffffffff            iiiiiiii",
+    }
 
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
       dashboard.button("SPC fm", "  > Toggle file explorer", "<cmd>Explore<CR>"),
+      dashboard.button("SPC ee", "󰱼  > Find File", "<cmd>NvimTreeToggle<CR>"),
       dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
       dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
